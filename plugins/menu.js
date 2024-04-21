92d53a4d026bef445e3c2a2233bad6442e0da8ad
vermilaz <167378572+vermilaz@users.noreply.github.com>
2024-04-21 12:09:15 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -138,7 +138,7 @@ contextInfo: {
 externalAdReply: {
 title: 'REVEN ZENIN🧡🎗️',
 body: "Welcome To Bot Reven🧡🗝️",
-thumbnailUrl: 'https://telegra.ph/file/aff44541eddd657f2dbac.mp4',
+thumbnailUrl: 'https://telegra.ph/file/164da7a7d64284eeffb38.jpg',
 sourceUrl: 'https://www.instagram.com/alyaa__zenin?igsh=OGQ5ZDc2ODk2ZA==',
 mediaType: 1,
 renderLargerThumbnail: true
